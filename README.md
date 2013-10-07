414fac4dbe1fbd0f5613b9b4d4807b74c83c4b5c
Masahiro Honma <hiratara@cpan.org>
2013-10-08 08:49:53 +0900

    the 1stversion

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+MyTestTest - It's new $module
+
+# SYNOPSIS
+
+    use MyTestTest;
+
+# DESCRIPTION
+
+MyTestTest is ...
+
+# LICENSE
+
+Copyright (C) Masahiro Honma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro Honma <hiratara@cpan.org>
---
+++ lib/MyTestTest.pm
@@ -0,0 +1,39 @@
+package MyTestTest;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+MyTestTest - It's new $module
+
+=head1 SYNOPSIS
+
+    use MyTestTest;
+
+=head1 DESCRIPTION
+
+MyTestTest is ...
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Honma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Honma E<lt>hiratara@cpan.orgE<gt>
+
+=cut
+
